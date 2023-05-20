e0a7e7c1eef0a2b6074012fc515d2c517aa0828e
Tóth Levente <levee@stud.uni-obuda.hu>
2023-05-20 19:17:11 +0200

    3 controller done

--- CTWO80_HFT_2022232.Endpoint/Controllers/PlayerController.cs
+++ CTWO80_HFT_2022232.Endpoint/Controllers/PlayerController.cs
@@ -1,11 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CTWO80_HFT_2022232.Logic;
+using CTWO80_HFT_2022232.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace CTWO80_HFT_2022232.Endpoint.Controllers
 {
 
-
+    [Route("[controller]")]
+    [ApiController]
     public class PlayerController:ControllerBase
     {
+        IPlayerLogic logic;
 
+        public PlayerController(IPlayerLogic logic)
+        {
+            this.logic = logic;
+        }
+
+
+        [HttpGet]
+        public IEnumerable<Player> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public Player Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Player value)
+        {
+            this.logic.Create(value);
+        }
+
+        [HttpPut]
+        public void Update([FromBody] Player value)
+        {
+            this.logic.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
     }
 }
