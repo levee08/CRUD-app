5ae5532a9b0b980885f3b6331092c303d4e72fec
Tóth Levente <levee@stud.uni-obuda.hu>
2023-05-20 19:12:37 +0200

    One controller done

---
+++ CTWO80_HFT_2022232.Endpoint/Controllers/FootballTeamController.cs
@@ -0,0 +1,50 @@
+﻿using CTWO80_HFT_2022232.Logic;
+using CTWO80_HFT_2022232.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CTWO80_HFT_2022232.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FootballTeamController
+    {
+
+        IFootballTeamLogic logic;
+
+        public FootballTeamController(IFootballTeamLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<FootballTeam> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public FootballTeam Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] FootballTeam value)
+        {
+            this.logic.Create(value);
+        }
+
+        [HttpPut]
+        public void Update([FromBody] FootballTeam value)
+        {
+            this.logic.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
+    }
+}
---
+++ CTWO80_HFT_2022232.Endpoint/Controllers/ManagerController.cs
@@ -0,0 +1,7 @@
+﻿namespace CTWO80_HFT_2022232.Endpoint.Controllers
+{
+    public class ManagerController
+    {
+
+    }
+}
