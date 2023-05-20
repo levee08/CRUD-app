162ff990742b495753bcbf20c2a32e77b6faef7d
Tóth Levente <levee@stud.uni-obuda.hu>
2023-05-20 20:31:10 +0200

    checkpoint

--- CTWO80_HFT_2022232.Client/Program.cs
+++ CTWO80_HFT_2022232.Client/Program.cs
@@ -1,31 +1,99 @@
-﻿using CTWO80_HFT_2022232.Models;
+﻿using ConsoleTools;
+using CTWO80_HFT_2022232.Models;
 using CTWO80_HFT_2022232.Repository;
 using CTWO80_HFT_2022232.Repository.ModelRepositories;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 
 namespace CTWO80_HFT_2022232.Client
 {
     internal class Program
     {
+        static RestService rest;
+        static void Create(string entity)
+        {
+            if (entity == "FootballTeam")
+            {
+                Console.Write("Enter FootballTeam Name: ");
+                string name = Console.ReadLine();
+                rest.Post(new FootballTeam() { FootballTeamName = name }, "actor");
+            }
+        }
+        static void List(string entity)
+        {
+            if (entity == "FootballTeam")
+            {
+                List<FootballTeam> actors = rest.Get<FootballTeam>("FootballTeam");
+                foreach (var item in actors)
+                {
+                    Console.WriteLine(item.FootballTeamId + ": " + item.FootballTeamName);
+                }
+            }
+            Console.ReadLine();
+        }
+        static void Update(string entity)
+        {
+            if (entity == "FootballTeam")
+            {
+                Console.Write("Enter FootballTeam's id to update: ");
+                int id = int.Parse(Console.ReadLine());
+                FootballTeam one = rest.Get<FootballTeam>(id, "FootballTeam");
+                Console.Write($"New name [old: {one.FootballTeamName}]: ");
+                string name = Console.ReadLine();
+                one.FootballTeamName = name;
+                rest.Put(one, "FootballTeam");
+            }
+        }
+        static void Delete(string entity)
+        {
+            if (entity == "FootballTeam")
+            {
+                Console.Write("Enter FootballTeam's id to delete: ");
+                int id = int.Parse(Console.ReadLine());
+                rest.Delete(id, "FootballTeam");
+            }
+        }
         static void Main(string[] args)
         {
-          
-            LeaugeDbContext ctx =new LeaugeDbContext();
 
-            ctx.Managers.Select(t => t.FootballTeams);
-            ctx.FootballTeams.Select(t => t.TrophiesWon);
-            ctx.Players.Select(t => t.FootballTeam);
+            rest = new RestService("http://localhost:53910/", "FootballTeam");
 
-            IRepository<Manager> repository = new ManagerRepository(new LeaugeDbContext());
-            IRepository<FootballTeam> trepository = new FootballTeamRepository(new LeaugeDbContext());
-            IRepository<Player> prepository = new PlayerRepository(new LeaugeDbContext());
+            var FootballteamSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => List("FootballTeam"))
+                .Add("Create", () => Create("FootballTeam"))
+                .Add("Delete", () => Delete("FootballTeam"))
+                .Add("Update", () => Update("FootballTeam"))
+                .Add("Exit", ConsoleMenu.Close);
 
-            var items = repository.ReadAll().ToArray();
-            var itemek = trepository.ReadAll().ToArray();
-            var itemeks = prepository.ReadAll().ToArray();
+            var managerSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => List("Manager"))
+                .Add("Create", () => Create("Manager"))
+                .Add("Delete", () => Delete("Manager"))
+                .Add("Update", () => Update("Manager"))
+                .Add("Exit", ConsoleMenu.Close);
 
-            
+            var playerSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => List("Player"))
+                .Add("Create", () => Create("Player"))
+                .Add("Delete", () => Delete("Player"))
+                .Add("Update", () => Update("Player"))
+                .Add("Exit", ConsoleMenu.Close);
+
+         
+
+
+            var menu = new ConsoleMenu(args, level: 0)
+                
+                .Add("FootballTeam", () => FootballteamSubMenu.Show())
+                .Add("Manager", () => managerSubMenu.Show())
+                .Add("Player", () => playerSubMenu.Show())
+                .Add("Exit", ConsoleMenu.Close);
+
+            menu.Show();
+
+
 
 
         }
