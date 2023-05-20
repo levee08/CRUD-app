fa7f81238ab9daba61ab9ca08a7ccf9243484c11
Tóth Levente <levee@stud.uni-obuda.hu>
2023-05-20 19:38:47 +0200

    Controller manager repaired

--- CTWO80_HFT_2022232.Endpoint/Controllers/ManagerController.cs
+++ CTWO80_HFT_2022232.Endpoint/Controllers/ManagerController.cs
@@ -3,12 +3,14 @@ using CTWO80_HFT_2022232.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
 namespace CTWO80_HFT_2022232.Endpoint.Controllers
 {
 
     [Route("[controller]")]
     [ApiController]
-    public class ManagerController:ControllerBase
+    public class ManagerController : ControllerBase
     {
 
         IManagerLogic logic;
