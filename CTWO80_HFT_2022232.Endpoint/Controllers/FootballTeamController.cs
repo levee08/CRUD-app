a0a052fc103e15b28dd903427804f613334a9c7c
Tóth Levente <levee@stud.uni-obuda.hu>
2023-05-20 19:37:08 +0200

    Startup half ConfigureServices done

--- CTWO80_HFT_2022232.Endpoint/Controllers/FootballTeamController.cs
+++ CTWO80_HFT_2022232.Endpoint/Controllers/FootballTeamController.cs
@@ -3,48 +3,52 @@ using CTWO80_HFT_2022232.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
 namespace CTWO80_HFT_2022232.Endpoint.Controllers
 {
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class FootballTeamController : ControllerBase
     {
+      
 
-        IFootballTeamLogic logic;
+            IFootballTeamLogic logic;
 
-        public FootballTeamController(IFootballTeamLogic logic)
-        {
-            this.logic = logic;
-        }
+            public FootballTeamController(IFootballTeamLogic logic)
+            {
+                this.logic = logic;
+            }
 
-        [HttpGet]
-        public IEnumerable<FootballTeam> ReadAll()
-        {
-            return this.logic.ReadAll();
-        }
+            [HttpGet]
+            public IEnumerable<FootballTeam> ReadAll()
+            {
+                return this.logic.ReadAll();
+            }
 
-        [HttpGet("{id}")]
-        public FootballTeam Read(int id)
-        {
-            return this.logic.Read(id);
-        }
+            [HttpGet("{id}")]
+            public FootballTeam Read(int id)
+            {
+                return this.logic.Read(id);
+            }
 
-        [HttpPost]
-        public void Create([FromBody] FootballTeam value)
-        {
-            this.logic.Create(value);
-        }
+            [HttpPost]
+            public void Create([FromBody] FootballTeam value)
+            {
+                this.logic.Create(value);
+            }
 
-        [HttpPut]
-        public void Update([FromBody] FootballTeam value)
-        {
-            this.logic.Update(value);
-        }
+            [HttpPut]
+            public void Update([FromBody] FootballTeam value)
+            {
+                this.logic.Update(value);
+            }
 
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-            this.logic.Delete(id);
-        }
+            [HttpDelete("{id}")]
+            public void Delete(int id)
+            {
+                this.logic.Delete(id);
+            }
+        
     }
 }
--- CTWO80_HFT_2022232.Endpoint/Startup.cs
+++ CTWO80_HFT_2022232.Endpoint/Startup.cs
@@ -1,3 +1,7 @@
+using CTWO80_HFT_2022232.Logic;
+using CTWO80_HFT_2022232.Models;
+using CTWO80_HFT_2022232.Repository;
+using CTWO80_HFT_2022232.Repository.ModelRepositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -7,6 +11,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace CTWO80_HFT_2022232.Endpoint
 {
@@ -16,6 +25,23 @@ namespace CTWO80_HFT_2022232.Endpoint
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<LeaugeDbContext>();
+
+            services.AddTransient<IRepository<FootballTeam>, FootballTeamRepository>();
+            services.AddTransient<IRepository<Manager>, ManagerRepository>();
+            services.AddTransient<IRepository<Player>, PlayerRepository>();
+            
+
+            services.AddTransient<IFootballTeamLogic, FootballTeamLogic>();
+            services.AddTransient<IManagerLogic, ManagerLogic>();
+            services.AddTransient<IPlayerLogic, PlayerLogic>();
+            
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CTWO80_HFT_2022232.Endpoint", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
