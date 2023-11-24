1a5c86221c69e00a00496022a7e174e10af49442
Tóth Levente <levee@stud.uni-obuda.hu>
2023-11-24 14:23:00 +0100

    Team all Crud JS done

--- TeamDbApp.JSClient/wwwroot/script.js
+++ TeamDbApp.JSClient/wwwroot/script.js
@@ -1,14 +1,52 @@
 ﻿let teams = [];
+let connection = null;
+let teamtoUpdateID = -1;
 getdata();
+setupSignalR();
+
+function setupSignalR() {
+    connection = new signalR.HubConnectionBuilder()
+        .withUrl("http://localhost:29829/hub")
+        .configureLogging(signalR.LogLevel.Information)
+        .build();
+
+    connection.on("footballTeamCreated", (user, message) => {
+                getdata();
+    });
+    connection.on("footballTeamDeleted", (user, message) => {
+        getdata();
+    });
+    connection.on("footballTeamUpdated", (user, message) => {
+        getdata();
+    });
+
+
+    connection.onclose
+        (async () => {
+            await start();
+        });
+    start();
+
+}
 async function getdata() {
     await fetch('http://localhost:29829/FootballTeam')
         .then(x => x.json())
         .then(y => {
             teams = y;
-            console.log(teams);
+           // console.log(teams);
             display();
         });
 }
+
+async function start() {
+    try {
+        await connection.start();
+        console.log("SignalR Connected.");
+    } catch (err) {
+        console.log(err);
+        setTimeout(start, 5000);
+    }
+};
 
 
 function display() {
@@ -24,10 +62,18 @@ function display() {
             + t.footballTeamName + "</td><td>"
             + t.currentPlacement + "</td><td>"
         + t.trophiesWon + "</td><td>"
-        +`<button type="button" onclick="remove(${t.footballTeamId})">Delete</button>`
+            + `<button type="button" onclick="remove(${t.footballTeamId})">Delete</button>`
+        + `<button type="button" onclick="ShowUpdate(${t.footballTeamId})">Update</button>`
             "</tr > ";
     });
 
+}
+function ShowUpdate(id) {
+    document.getElementById('teamnametoupdate').value = teams.find(t => t['footballTeamId'] == id)['footballTeamName'];
+    document.getElementById('teamplacementtoupdate').value = teams.find(t => t['footballTeamId'] == id)['currentPlacement'];
+    document.getElementById('teamtrophiestoupdate').value = teams.find(t => t['footballTeamId'] == id)['trophiesWon'];
+    document.getElementById('updateformdiv').style.display = 'flex';
+    teamtoUpdateID = id;
 }
 
 function remove(id) {
@@ -66,4 +112,26 @@ function create() {
             console.error('Error: ', error);
         });
         
+}
+function update() {
+    document.getElementById('updateformdiv').style.display = 'none';
+    let teamname = document.getElementById('teamnametoupdate').value;
+    let teamplacement = document.getElementById('teamplacementtoupdate').value;
+    let teamthrophies = document.getElementById('teamtrophiestoupdate').value;
+    fetch('http://localhost:29829/FootballTeam', {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json', },
+        body: JSON.stringify(
+            {
+                footballTeamName: teamname, currentPlacement: teamplacement, trophiesWon: teamthrophies,footballTeamId:teamtoUpdateID
+            })
+    }).then(response => response)
+        .then(data => {
+            console.log('Success: ', data);
+            getdata();
+        })
+        .catch((error) => {
+            console.error('Error: ', error);
+        });
+
 }
